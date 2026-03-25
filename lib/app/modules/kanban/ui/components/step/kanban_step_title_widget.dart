a19592670ad88a9f2ea5304b839be56a62e2fe3f
Evandro Mapa <evandromapa@gmail.com>
2026-03-24 22:29:34 -0300

    fix: correção de sintaxe no KanbanStepTitleWidget (removendo '...')

--- lib/app/modules/kanban/ui/components/step/kanban_step_title_widget.dart
+++ lib/app/modules/kanban/ui/components/step/kanban_step_title_widget.dart
@@ -1,9 +1,32 @@
-import 'package:aco_plus/app/modules/pedido/ui/pedido_import_pdf_dialog.dart';
+import 'package:aco_plus/app/core/client/firestore/collections/pedido/models/pedido_model.dart';
+import 'package:aco_plus/app/core/client/firestore/collections/step/models/step_model.dart';
+import 'package:aco_plus/app/core/components/h.dart';
+import 'package:aco_plus/app/core/components/w.dart';
+import 'package:aco_plus/app/core/enums/sort_step_type.dart';
+import 'package:aco_plus/app/core/extensions/double_ext.dart';
 import 'package:aco_plus/app/core/services/notification_service.dart';
+import 'package:aco_plus/app/core/utils/app_css.dart';
+import 'package:aco_plus/app/modules/kanban/kanban_controller.dart';
+import 'package:aco_plus/app/modules/kanban/kanban_view_model.dart';
+import 'package:aco_plus/app/modules/pedido/ui/pedido_import_pdf_dialog.dart';
 import 'package:flutter/material.dart';
 
 class KanbanStepTitleWidget extends StatelessWidget {
-...
+  final KanbanUtils utils;
+  final StepModel step;
+  final List<PedidoModel> pedidos;
+  const KanbanStepTitleWidget(this.utils, this.step, this.pedidos, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      dense: false,
+      minTileHeight: 30,
+      tilePadding: const EdgeInsets.only(left: 8),
+      childrenPadding: const EdgeInsets.symmetric(horizontal: 8),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
           PopupMenuButton<int>(
             tooltip: 'Criar Cartão',
             icon: const Icon(Icons.add, color: Colors.blue, size: 20),
