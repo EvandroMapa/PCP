5fdb2a9b8f0f2e5b271127ee922853db88036444
Evandro Mapa <evandromapa@gmail.com>
2026-04-07 18:24:35 -0300

    backup

--- lib/app/core/services/backup_download_service/backup_download_web.dart
+++ lib/app/core/services/backup_download_service/backup_download_web.dart
@@ -5,22 +5,18 @@ import 'dart:typed_data';
 import 'package:aco_plus/app/core/services/supabase_storage_service.dart';
 
 Future<bool> backupDownload(String name, String path, Uint8List bytes) async {
-  try {
-    await SupabaseStorageService.uploadFile(
-      name: name,
-      bytes: bytes,
-      mimeType: 'application/json',
-      path: '',
-      bucket: 'backups',
-    );
-    html.AnchorElement(
-        href:
-            "data:application/octet-stream;charset=utf-16le;base64,${base64.encode(bytes)}",
-      )
-      ..setAttribute("download", name)
-      ..click();
-    return true;
-  } catch (_) {
-    return false;
-  }
+  await SupabaseStorageService.uploadFile(
+    name: name,
+    bytes: bytes,
+    mimeType: 'application/json',
+    path: '',
+    bucket: 'backups',
+  );
+  html.AnchorElement(
+      href:
+          "data:application/octet-stream;charset=utf-16le;base64,${base64.encode(bytes)}",
+    )
+    ..setAttribute("download", name)
+    ..click();
+  return true;
 }
